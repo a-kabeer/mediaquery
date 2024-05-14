56b8abb677dff8bc544ff94006cb125287ba4624
a-kabeer <a.kabeer1991@gmail.com>
2024-05-14 14:30:51 +0500

    Media Query

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,9 +18,46 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-        body: Container(
-          width: 100,
-          height: 100,color: Colors.amber,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.1,
+                height: MediaQuery.of(context).size.height * 0.1,
+                color: Colors.amber,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.1,
+                height: MediaQuery.of(context).size.height * 0.1,
+                color: Colors.black,
+              ),
+              Stack(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.green,
+                      radius: 10,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
